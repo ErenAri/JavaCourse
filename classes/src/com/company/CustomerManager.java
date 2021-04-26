006d5f3698e7a6efe2dc7f0e5609c1fe10f021a2
Eren Arı <erenari27@gmail.com>
2021-04-26 17:06:23 +0300

    .

---
+++ classes/src/com/company/CustomerManager.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class CustomerManager {
+    public void Add(){
+        System.out.println("Customer added");
+    }
+
+    public void Remove(){
+        System.out.println("Customer removed");
+    }
+
+    public void Update(){
+        System.out.println("Customer updated");
+    }
+}
