a910049b7155466571b211c6209e0f96d03b9649
Eren Arı <erenari27@gmail.com>
2021-04-26 16:47:25 +0300

    .

--- methodsV2/src/com/company/Main.java
+++ methodsV2/src/com/company/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println(newMessage);
         int number = plus(5,7);
         System.out.println(number);
+        int total = plus2(5,6,7,3,4,1,1);
     }
 
     public static void add(){
@@ -24,6 +25,14 @@ public class Main {
 
     public static int plus(int number1,int number2){
         return number1+number2;
+    }
+
+    public static int plus2(int... numbers){
+        int total = 0;
+        for (int number:numbers){
+            total+=number;
+        }
+        return total;
     }
 
     public static String giveCity(){
