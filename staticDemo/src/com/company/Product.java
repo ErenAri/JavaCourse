ac7e1109394b69b763a3b2a081a2330ef054b81f
Eren Arı <erenari27@gmail.com>
2021-05-04 21:57:47 +0300

    .

---
+++ staticDemo/src/com/company/Product.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.stream.StreamSupport;
+
+public class Product {
+    int id;
+    String name;
+    double price;
+}
---
+++ staticDemo/src/com/company/ProductValidator.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ProductValidator {
+    public static boolean isValid(Product product){
+
+        if (product.price>0 && !product.name.isEmpty()){
+            return true;
+        }else {
+            return false;
+        }
+
+
+    }
+}
